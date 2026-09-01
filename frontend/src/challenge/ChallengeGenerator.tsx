05ba499fea9087728f6a3f5b05dc6dc27453fafb
Jayzuss1023 <jesus.flo369@gmail.com>
2026-09-01 00:00:49 -0500

    fetch quota to frontend

--- frontend/src/challenge/ChallengeGenerator.tsx
+++ frontend/src/challenge/ChallengeGenerator.tsx
@@ -1,4 +1,26 @@
+import { useEffect, useState } from "react";
+import { useApi } from "../utils/api";
+
 export default function ChallengeGenerator() {
+  const [difficulty, setDifficulty] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const [quota, setQuota] = useState(null);
+
+  const apiUse = useApi();
+
+  useEffect(() => {
+    fetchQuota();
+  }, []);
+
+  // Declare quota. Required to move forward with challenge generation
+  const fetchQuota = async () => {
+    try {
+      const data = await apiUse.makeRequest("quota");
+      console.log("DATA", data);
+    } catch (err) {}
+  };
+
   return (
     <div>
       <div>Generator</div>
